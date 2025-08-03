92e7465ab22bac38ab221af353d01b1df6836a2a
Emaj7th <gideonmarken@gmail.com>
2025-08-02 23:04:56 -0700

    new piano samples and patterns
    new piano samples and patterns

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Configuration Constants
 const scalesChordsURL = 'data/scales_chords.csv';
 const chordProgressionsURL = 'data/chord_progressions.csv';
+const bassPatternsURL = 'data/bass_patterns.csv';
 const keyboardRhythmsURL = 'data/keyboard_rhythms.csv';
 let keyboardRhythmsData = [];
 const drumPatternsURL = 'data/drum_patterns.json';
@@ -303,39 +304,39 @@ const reverb = new Tone.Reverb({
  
 const sampler = new Tone.Sampler({
   urls: {
-    A0: "A0.mp3",
-    C1: "C1.mp3",
-    "D#1": "Ds1.mp3",
-    "F#1": "Fs1.mp3",
     A1: "A1.mp3",
-    C2: "C2.mp3",
-    "D#2": "Ds2.mp3",
-    "F#2": "Fs2.mp3",
-    A2: "A2.mp3",
+    B1: "B1.mp3",
+    Db2: "Db2.mp3",
+    Eb2: "Eb2.mp3",
+    F2: "F2.mp3",
+    Ab2: "Ab2.mp3",
+    Bb2: "Bb2.mp3",
     C3: "C3.mp3",
-    "D#3": "Ds3.mp3",
-    "F#3": "Fs3.mp3",
+    D3: "D3.mp3",
+    F3: "F3.mp3",
     A3: "A3.mp3",
     C4: "C4.mp3",
-    "D#4": "Ds4.mp3",
-    "F#4": "Fs4.mp3",
+    D4: "D4.mp3",
+    E4: "E4.mp3",
+    Gb4: "Gb4.mp3",
     A4: "A4.mp3",
-    C5: "C5.mp3",
-    "D#5": "Ds5.mp3",
-    "F#5": "Fs5.mp3",
+    B4: "B4.mp3",
+    D5: "D5.mp3",
+    Gb5: "Gb5.mp3",
     A5: "A5.mp3",
-    C6: "C6.mp3",
-    "D#6": "Ds6.mp3",
-    "F#6": "Fs6.mp3",
-    A6: "A6.mp3",
-    C7: "C7.mp3",
-    "D#7": "Ds7.mp3",
-    "F#7": "Fs7.mp3",
+    B5: "B5.mp3",
+    D6: "D6.mp3",
+    E6: "E6.mp3",
+    Ab6: "Ab6.mp3",
+    B6: "B6.mp3",
+    D7: "D7.mp3",
+    F7: "F7.mp3",
+    G7: "G7.mp3",
     A7: "A7.mp3",
-    C8: "C8.mp3",
+    B7: "B7.mp3"
   },
   release: 1,
-  baseUrl: "https://tonejs.github.io/audio/salamander/",     //"samples/piano/",
+  baseUrl: "../samples/piano/",
 }).connect(reverb);  // Connect to reverb instead of toDestination()
 
 // Add after piano sampler setup
@@ -653,11 +654,10 @@ class ProgressionLooper {
       });
       const chordIndex = Math.floor(currentBeat / 4);
       const beatInChord = currentBeat % 4;
-      //const beatInChord = (currentBeat % 16) / 4;  // Convert sixteenth notes back to quarter notes
-      const eighthNoteIndex = currentBeat * 2;  // Convert quarter notes to eighth notes
-      
+      //const eighthNoteIndex = currentBeat * 2;  // Convert quarter notes to eighth notes?
+      const sixteenthNoteIndex = currentBeat * 4; 
       // Process drum pattern
-      this.processDrumPattern(time, eighthNoteIndex);
+      this.processDrumPattern(time, sixteenthNoteIndex);
 
       // Clear previous highlight
       for (let i = 0; i < 4; i++) {
@@ -665,7 +665,7 @@ class ProgressionLooper {
         if (container) {
           container.style.backgroundColor = '';
         }
-      }
+      } 
   
       // Highlight current chord
       const currentContainer = document.getElementById(`chord-container-${chordIndex}`);
